4d97103dab698ac8fefbe697c953ce6a250e93a2
Douglas <frc200_2@hotmail.com>
2020-10-25 00:56:07 -0300

    bubble sort

--- src/BSADouglas.java
+++ src/BSADouglas.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 public class BSADouglas {
 	public static void main(String[] args) {
 
-		HashMap<>
+
 	}
 
-	private int recursiveBinarySearch(int[] array, int startPos, int finalPos, int numToFind) {
+	public int recursiveBinarySearch(int[] array, int startPos, int finalPos, int numToFind) {
 			int arrayMiddlePos = (startPos+finalPos)/2;
 			if (array[arrayMiddlePos] == numToFind) return arrayMiddlePos;
 			if (array[arrayMiddlePos] > numToFind) {
--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,13 +1,25 @@
-public class BubbleSort {
+import java.util.Arrays;
+
+class BubbleSort {
 	public static void main(String[] args) {
-		int[] newArray = {1,4,7,2,6};
+		int[] newArray = {5,3,1,7,8,2,4,6};
 
-		this.recursiveBubbleSort(newArray);
+		int[] sortedArray = recursiveBubbleSort(newArray, 1, newArray.length);
 
-		System.out.println(newArray);
+		System.out.println(Arrays.toString(sortedArray));
 	}
 
-	public void recursiveBubbleSort(int[] arrayToSort, int currentIndex) {
+	public static int[] recursiveBubbleSort(int[] arrayToSort, int currentIndex, int endIndex) {
+		return bubbleSort(arrayToSort, currentIndex, endIndex);
+	}
 
+	public static int[] bubbleSort(int[] arrayToSort, int currentIndex, int endIndex) {
+		if (endIndex == 1) return arrayToSort;
+		if (currentIndex == endIndex) return bubbleSort(arrayToSort, 1, endIndex-1);
+		if (arrayToSort[currentIndex-1] < arrayToSort[currentIndex]) return bubbleSort(arrayToSort, currentIndex+1, endIndex);
+		int buff = arrayToSort[currentIndex-1];
+		arrayToSort[currentIndex-1] = arrayToSort[currentIndex];
+		arrayToSort[currentIndex] = buff;
+		return bubbleSort(arrayToSort, currentIndex+1, endIndex);
 	}
 }
